f9faf5428c39600e0b6ec3a05500a335619dc70b
alexarlord-boop <rezerw0.1@yandex.ru>
2023-06-06 20:33:55 +0300

    BTC-2023 Added all_events.php

---
+++ pages/all_events.php
@@ -0,0 +1,18 @@
+<?php
+include "../components/navbar.php";
+include "../components/head_template.php";
+require "../utility.php";
+session_start();
+$_SESSION['searchFor'] = $_SESSION['userType'];
+$navbar = returnNavBar(null);
+$body = <<<HTML
+   
+    <div class="display-4"><i class="fa  fa-search"></i> Search for Events</div>
+    
+   
+HTML;
+
+
+echo page($navbar, $body);
+
+?>
