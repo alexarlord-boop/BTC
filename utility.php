f478531b4e0ef7d3352a07ad9935b179bee7d2ac
alexarlord-boop <rezerw0.1@yandex.ru>
2023-06-06 15:20:27 +0300

    BTC-2023 Updated page and footer

--- utility.php
+++ utility.php
@@ -10,49 +10,66 @@ function page($navbar, $body) {
             <html lang="en">
             
             <body>
-            $navbar
-                
-            <div class="container mt-2">
-                $body
-                
-                <div class="container">
-              <footer class="footer d-flex flex-wrap justify-content-between align-items-center py-3 my-2 border-top">
-                
-                <ul class="nav col-md-8 justify-content-center list-unstyled d-flex">
-                  <li class="ms-3"><a class="text-body-secondary" href="#"><i class="fa-brands fa-2x fa-github"></i></a></li>
-                  <li class="ms-3"><a class="text-body-secondary" href="#"><i class="fa-brands fa-2x fa-linkedin"></i></a></li>
-                
-                    <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                    <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-                  </a>
-                  <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Company, Inc</span>
-                </ul>
-       
-                <div class='col-md-4'>
-                    <ul class='list-group h6 align-items-end mr-5'>
-                        <a class="stretched-link text-secondary" href="#">Art</a>
-                        <a class="stretched-link text-secondary" href="#">Tech</a>
-                        <a class="stretched-link text-secondary" href="#">Health</a>
-                        <a class="stretched-link text-secondary" href="#">Education</a>
-                        <a class="stretched-link text-secondary" href="#">Environment</a>
-                    </ul>
+                $navbar
+                    
+                <div class="container mt-2 h-75">
+                    $body
                 </div>
-    
-              </footer>
-            </div>
-            </div>
-             
-            <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasWithBothOptionsLabel">
-              <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-              <div class="offcanvas-body">
-                <p>Try scrolling the rest of the page to see this option in action.</p>
-              </div>
-            </div>              
-              
-              
+                
+                <!-- FOOTER -->
+                <div class="container">
+                      <footer class="">
+                        <div class="row">
+
+                        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                          <p>© 2022 Company, Inc. All rights reserved.</p>
+                          <ul class="list-unstyled d-flex">
+                            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+                            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+                            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+                          </ul>
+                        </div>
+                      </footer>
+                </div>
+                 
+                <!-- OFFCANVAS -->
+                <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasWithBothOptionsLabel">
+                  <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Follow & Support</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                  </div>
+                  <div class="offcanvas-body">
+                     
+                    
+                          <div class="col-md-12 mb-5">
+                            <form>
+                              <h5>Subscribe to our newsletter</h5>
+                              <p>Monthly digest of what's new and exciting from us.</p>
+                              <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                                <label for="newsletter1" class="visually-hidden">Email address</label>
+                                <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                                <button class="btn btn-primary" type="button">Subscribe</button>
+                              </div>
+                            </form>
+                          </div>
+                          
+                          
+                          <div class="col-2 mt-5 text-right">
+                            <h5>Section</h5>
+                            <ul class="nav flex-column">
+                              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                            </ul>
+                          </div>
+                          
+                        </div>
+                  </div>
+                </div>              
+                  
+                  
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
             </body>
