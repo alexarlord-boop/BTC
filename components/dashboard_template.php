f4d4c86a9224fcd3900df3137b9759f76eb0b722
alexarlord-boop <rezerw0.1@yandex.ru>
2023-06-08 14:44:14 +0300

    BTC-2023 Added title template

--- components/dashboard_template.php
+++ components/dashboard_template.php
@@ -12,10 +12,11 @@ function returnDashboard($title, $pointer, $content)
 //    pre($pointer);
 
     $navbar = returnNavBar($pointer);
+    $title = pageTitle("laptop", $title);
     $body = <<<HTML
            
            
-           <h1 class="display-6 text-center p-3"><i class="fa fa-1x fa-laptop"></i> $title</h1>
+           $title
            <div class="mt-3">
             $content
            </div>
